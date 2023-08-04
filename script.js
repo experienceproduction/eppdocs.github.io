11d295cbd1ea2f0848063033217c4b8f88e371ec
EXPERIENCE <138899642+experienceproduction@users.noreply.github.com>
2023-08-04 01:31:15 +0000

    pdfCreate

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-import { jsPDF } from "jspdf";
-
-const doc = new jsPDF();
-
-doc.text("Hello World!", 20, 20);
-doc.save("a4.pdf");
+window.jsPDF = window.jspdf.jsPDF;
+var docPDF = new jsPDF();
+function print() {
+    var elementHTML = document.querySelector("#contentToPrint");
+    docPDF.html(elementHTML, {
+        callback: function(docPDF) {
+            docPDF.save('HTML Linuxhint web page.pdf');
+        }
+        x: 15,
+        y: 15,
+        width: 170,
+        windowWidth: 650
+    });
+}
