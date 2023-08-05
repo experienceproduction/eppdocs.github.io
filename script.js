ceadc3266f358724cf5b340e3efdb4a803cf3f8b
EXPERIENCE <138899642+experienceproduction@users.noreply.github.com>
2023-08-05 18:35:16 +0000

    link-to-google-sheet

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.jsPDF = window.jspdf.jsPDF;
+/* window.jsPDF = window.jspdf.jsPDF;
 var docPDF = new jsPDF();
 function print() {
     var elementHTML = document.querySelector("#contentToPrint");
@@ -11,4 +11,20 @@ function print() {
         width: 170,
         windowWidth: 650
     });
-}
+} */
+
+var $form = $('form#test-form'),
+    // url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
+    url = 'https://script.google.com/macros/s/AKfycbzOVlMHIAU4ielb0i_YnrecYvuRhQKcbuRXulYuRDa0rIB4cxKcPx88qR8NW_3Af7pHZQ/exec'
+
+$('#submit-form').on('click', function (e) {
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: $form.serializeObject()
+    }).success(
+        // do something
+    );
+})
